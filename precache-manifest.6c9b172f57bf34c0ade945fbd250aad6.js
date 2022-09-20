2fb001866dd050848bbc2cc37fa5bb7e0a80b022
idakukimiya <kukimiya.ida@gmail.com>
2022-09-20 11:35:53 -0400

    Updates

---
+++ precache-manifest.6c9b172f57bf34c0ade945fbd250aad6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "22ec71139872cad523c7",
+    "url": "/idakukimiya/Ida_REACTportfolio/static/js/main.22ec7113.chunk.js"
+  },
+  {
+    "revision": "db419553f6400a028f2c",
+    "url": "/idakukimiya/Ida_REACTportfolio/static/js/1.db419553.chunk.js"
+  },
+  {
+    "revision": "535e09add6cc4338e2b0",
+    "url": "/idakukimiya/Ida_REACTportfolio/static/js/runtime~main.535e09ad.js"
+  },
+  {
+    "revision": "f35db62cd39406946f22e1d9bde1d57a",
+    "url": "/idakukimiya/Ida_REACTportfolio/index.html"
+  }
+];
