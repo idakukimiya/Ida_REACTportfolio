d1af499b930865fb1728f7c1da5918e680ebdc40
idakukimiya <kukimiya.ida@gmail.com>
2022-09-22 00:18:59 -0400

    add stylesheet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
 
-const App = () => <PortfolioContainer />;
+import React, {useState} from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import PortfolioContainer from "./Components/PortfolioContainer";
+import About from './Components/pages/About';
+import Portfolio from './Components/pages/Portfolio';
+import Contact from './Components/pages/Contact';
+import Resume from './Components/pages/Resume';
+
+function App () {
+
+    const [currentPage, setCurrentPage]= useState('About');
+
+    const renderPage = () => {
+        if(currentPage === 'About') {
+            return<About/>;
+        }
+        if(currentPage === 'Portfolio') {
+            return<Portfolio/>
+        }
+        if(currentPage === 'Contact') {
+            return<Contact/>
+        }
+        return<Resume/>
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+        return <div>
+            <Header handlePageChange={handlePageChange} currentPage={currentPage}/>
+            <PortfolioContainer renderPage={renderPage}/>
+            <Footer/>
+        </div>
+
+
+
+}
 
 export default App;
