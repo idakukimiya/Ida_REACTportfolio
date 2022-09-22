82e1d7b06363bfc29af7529cba841618cdf9f6f3
idakukimiya <kukimiya.ida@gmail.com>
2022-09-22 03:10:32 -0400

    add more images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from './images/github-icon.png';
+import LinkedInIcon from './images/linkedin-icon.png';
 
 
 function Footer() {
