b90e8002275059f00fad6cc0fb2825ea6c1f99f5
idakukimiya <kukimiya.ida@gmail.com>
2022-09-22 12:46:46 -0400

    build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import GitHubIcon from './image/Octocat.jpg';
-import LinkedInIcon from './image/linkedin-icon.png';
+// import GitHubIcon from './image/Octocat.jpg';
+// import LinkedInIcon from './image/linkedin-icon.png';
 
 
 function Footer() {
@@ -8,8 +8,8 @@ function Footer() {
     return (
         <div className="footer" id="footer">
         <ul>
-            <li><a href="https://github.com/idakukimiya" rel="noreferrer"><GitHubIcon className="icon"></GitHubIcon></a></li>
-            <li><a href="https://www.linkedin.com/in/idakukimiya/" rel="noreferrer"><LinkedInIcon className="icon"></LinkedInIcon></a></li>
+            {/* <li><a href="https://github.com/idakukimiya" rel="noreferrer"><GitHubIcon className="icon"></GitHubIcon></a></li> */}
+            {/* <li><a href="https://www.linkedin.com/in/idakukimiya/" rel="noreferrer"><LinkedInIcon className="icon"></LinkedInIcon></a></li> */}
         </ul>
     </div>
         );
